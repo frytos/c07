03c31d161673bfe8553c44ca14e5ed7cfbc6a574
Frytos <frytos@protonmail.com>
2024-02-28 00:05:12 +0100

    adds latest convert_base

--- ex04/ft_convert_base.c
+++ ex04/ft_convert_base.c
@@ -1,142 +1,163 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*   convert_to_base.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: argrouss <argrouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/23 03:54:13 by argrouss          #+#    #+#             */
-/*   Updated: 2024/02/23 10:54:42 by argrouss         ###   ########.fr       */
+/*   Created: 2024/02/24 22:15:47 by argrouss          #+#    #+#             */
+/*   Updated: 2024/02/24 22:15:52 by argrouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
 #include <stdlib.h>
+#include <limits.h>
 
+int		strpos(char *str, char c);
+int		ft_atoi_base(char *nbr, char *base_from);
+int		get_malloc_size(int n, int len_base);
+char	*ft_itoa_base(int n, char *base_to);
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to);
-char    *ft_strdup(char *src);
-// int	ft_strlen(char *str);
-int	len_in_base(long nb, char *base);
-int     check_base_and_size(char *str);
 
-int     ft_atoi_base(char *str, char *base);
-// int     check_n_size(char *base);
-int     ft_if_char_in_base_which(char *str, char *base, int base_nb);
-long    ft_add_coeff(char *str, char *base, int base_nb, long *ptr_u_long);
-void    ft_putnbr_base(int nbr, char *base, char *r);
-// void    ft_putchar(char c);
-void    get_and_print_significand(long long_nbr, int base_number, char *base, char *r);
-// char    convert_in_base(int significand, char *base);
-
-char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	int	arr_len[10];
-	//char	*n;
-	char	*r;
-	long	nb_int;
-
-	// arr_len[0] = ft_strlen(nbr);
-	if (check_base_and_size(base_from) < 0 || check_base_and_size(base_from) < 0)
-		return (NULL);
-	arr_len[1] = check_base_and_size(base_from);
-	arr_len[2] = check_base_and_size(base_to);
-	// n = ft_strdup(nbr);
-	nb_int = ft_atoi_base(nbr, base_from);
-	if (nb_int < INT_MIN || INT_MAX < nb_int)
-		return (NULL);
-	r = malloc(len_in_base(nb_int, base_to));
-	// if (r == NULL)
-	// 	return (NULL);
-	ft_putnbr_base(nb_int, base_to, r);	
-	return (r);
+	return (ft_itoa_base(ft_atoi_base(nbr, base_from), base_to));
 }
 
-int	len_in_base(long nb, char *base)
+char	*ft_itoa_base(int n, char *base_to)
 {
-	int	len_base;
-	int	len_nb;
+	int		i;
+	int		j;
+	int		len_base_to;
+	char	*nbr_converted;
 
-	len_nb = 1;
-	if (nb < 0)
+	len_base_to = strpos(base_to, 0);
+	i = get_malloc_size(n, len_base_to);
+	nbr_converted = (char *) malloc(i * sizeof(char));
+	if (nbr_converted == NULL)
+		return ("Error malloc()");
+	if (n < 0)
 	{
-		nb = - nb;
-		len_nb++;
+		nbr_converted[0] = '-';
+		n = -n;
 	}
-	len_base = 0;
-	while (base[len_base])
-		len_base++;
-	while (nb / len_base > 0)
+	nbr_converted[i - 1] = 0;
+	j = -1;
+	while (n > 0)
 	{
-		len_nb++;
-		nb /= len_base;
+		nbr_converted[i - 2 - ++j] = base_to[n % len_base_to];
+		n = n / len_base_to;
 	}
-	return (len_nb);
+	return (nbr_converted);
 }
 
-int check_base_and_size(char *str)
+int	ft_atoi_base(char *nbr, char *base_from)
 {
-    int len;
-    int search_index;
+	int	i;
+	int	sign;
+	int	n;
+	int	len_base_from;
 
-    len = 0;
-    while (str[len] != 0)
-    {
-        if (str[len] == 43 || str[len] == 45)
-            return (-3);
-        search_index = 1;
-        while (str[len + search_index] != 0)
-        {
-            if (str[len] == str[len + search_index])
-                return (-2);
-            search_index++;
-        }
-        len++;
-    }
-    if (len > 1)
-        return (len);
-    else
-        return (-1);
+	i = 0;
+	sign = 1;
+	n = 0;
+	while ((9 <= nbr[i] && nbr[i] <= 13) || nbr[i] == 32)
+		i++;
+	while (nbr[i] == '-' || nbr[i] == '+')
+	{
+		if (nbr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	len_base_from = strpos(base_from, 0);
+	while (nbr[i])
+	{
+		n = len_base_from * n + strpos(base_from, nbr[i]);
+		i++;
+	}
+	n *= sign;
+	return (n);
 }
 
-// char    *ft_strdup(char *src)
-// {
-//     int     index;
-//     int     len;
-//     char    *duplicate;
-// 
-//     len = 0;
-//     while (src[len])
-//         len++;
-//     duplicate = malloc((len + 1) * sizeof(char));
-//     if (duplicate == NULL)
-//         return (NULL);
-//     index = 0;
-//     while (src[index])
-//     {
-//         duplicate[index] = src[index];
-//         index++;
+int	get_malloc_size(int n, int len_base)
+{
+	int	size;
+	int	n_malloc;
+
+	size = 1;
+	if (n < 0)
+	{
+		n_malloc = -n;
+		size++;
+	}
+	else
+		n_malloc = n;
+	while (n_malloc != 0)
+	{
+		n_malloc = n_malloc / len_base;
+		size++;
+	}
+	return (size);
+}
+
+int	strpos(char *str, char c)
+{
+	int	pos;
+
+	pos = 0;
+	while (str[pos] != c)
+		pos++;
+	return (pos);
+}
+
+// int main(void) {
+//     char *input1 = "       -+-+---+--2147483647";
+//     char *base_from1 = "0123456789";
+//     char *base_to1 = "0123456789";
+
+//     char *input2 = "101010";
+//     char *base_from2 = "01";
+//     char *base_to2 = "0123456789";
+
+//     char *input3 = "F7A25";
+//     char *base_from3 = "0123456789ABCDEF";
+//     char *base_to3 = "0123456789";
+
+//     printf("CONVERSION\n\n");
+
+//     printf("Test 1:\n");
+//     printf("Input: %s\nBase From: %s\nBase To: %s\nResult: %s\n\n", 
+//             input1, base_from1, base_to1, 
+// 			ft_convert_base(input1, base_from1, base_to1));
+
+//     printf("Test 2:\n");
+//     printf("Input: %s\nBase From: %s\nBase To: %s\nResult: %s\n\n", 
+//             input2, base_from2, base_to2, 
+// 			ft_convert_base(input2, base_from2, base_to2));
+
+//     printf("Test 3:\n");
+//     printf("Input: %s\nBase From: %s\nBase To: %s\nResult: %s\n\n", 
+//             input3, base_from3, base_to3, 
+// 			ft_convert_base(input3, base_from3, base_to3));
+
+//     return 0;
+// }
+
+// int main(int argc, char *argv[]) {
+//     if (argc != 4) {
+//         printf("Usage: %s <input> <base_from> <base_to>\n", argv[0]);
+//         return 1;
 //     }
-//     duplicate[index] = 0;
-//     return (duplicate);
-// }
 
-// int	ft_strlen(char *str)
-// {
-// 	int	len;
-// 
-// 	len = 0;
-// 	while (str[len])
-// 		len++;
-// 	return(len);
-// }
+//     char *input = argv[1];
+//     char *base_from = argv[2];
+//     char *base_to = argv[3];
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    // (void)argv;
-    // printf("%s",ft_convert_base("ff", "0123456789abcdef", "01"));
-    printf("%s",ft_convert_base(argv[1], argv[2], argv[3]));
-    //ex : ./a.out 1458 0123456789 0123456789abcdef
-}
+//     printf("CONVERSION\n\n");
+//     printf("Input: %s\nBase From: %s\nBase To: %s\nResult: %s\n", 
+//             input, base_from, base_to, 
+// 			ft_convert_base(input, base_from, base_to));
+
+//     return 0;
+// }
