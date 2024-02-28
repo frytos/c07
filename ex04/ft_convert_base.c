ccb4b474c92f059d4623e54156c0ebcb036f1842
Frytos <frytos@protonmail.com>
2024-02-28 04:45:25 +0100

    adds testing

--- ex04/ft_convert_base.c
+++ ex04/ft_convert_base.c
@@ -16,52 +16,34 @@
 
 int		strpos(char *str, char c);
 long	ft_atoi_base(char *nbr, char *base_from);
-int		get_malloc_size(int n, int len_base);
+int		get_malloc_size(long n, int len_base);
 char	*ft_putnbr_base(long n, char *base_to);
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to);
 int 	chkbse_getsz(char *base);
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
-	if (chkbse_getsz(base_from) <= 1 || 
-		chkbse_getsz(base_to) <= 1)
-		return (NULL);
-	return (ft_putnbr_base(ft_atoi_base(nbr, base_from), base_to));
-}
-
-char	*ft_putnbr_base(long n, char *base_to)
-{
-	int		i;
-	int		j;
-	int		len_base_to;
-	char	*nbr_converted;
-
-	len_base_to = strpos(base_to, 0);
-	i = get_malloc_size(n, len_base_to);
-	nbr_converted = (char *) malloc(i * sizeof(char));
-	if (nbr_converted == NULL)
-		return (NULL);
-	if (n < 0)
+	//if (chkbse_getsz(base_from) <= 1 && chkbse_getsz(base_to) <= 1)
+	// if (chkbse_getsz(base_to) <= 1)
+	// {
+	// 	return (NULL);
+	// }
+	if (chkbse_getsz(base_to) <= 1 || chkbse_getsz(base_from) <= 1)
 	{
-		nbr_converted[0] = '-';
-		n = -n;
+		return (NULL);
 	}
-	nbr_converted[i - 1] = 0;
-	j = -1;
-	while (n > 0)
-	{
-		nbr_converted[i - 2 - ++j] = base_to[n % len_base_to];
-		n = n / len_base_to;
-	}
-	return (nbr_converted);
+	if (!(chkbse_getsz(base_from) <= 1 || chkbse_getsz(base_to) <= 1))
+		return (ft_putnbr_base(ft_atoi_base(nbr, base_from), base_to));
+
+	return (NULL);
 }
 
 long	ft_atoi_base(char *nbr, char *base_from)
 {
 	int		i;
-	int		sign;
+	long	sign;
 	long	n;
-	int		len_base_from;
+	long	len_base_from;
 
 	i = 0;
 	sign = 1;
@@ -75,7 +57,7 @@ long	ft_atoi_base(char *nbr, char *base_from)
 		i++;
 	}
 	len_base_from = strpos(base_from, 0);
-	while (nbr[i])
+	while (nbr[i] && strpos(base_from, nbr[i]) >=0)
 	{
 		n = len_base_from * n + strpos(base_from, nbr[i]);
 		i++;
@@ -84,17 +66,47 @@ long	ft_atoi_base(char *nbr, char *base_from)
 	return (n);
 }
 
-int	get_malloc_size(int n, int len_base)
+char	*ft_putnbr_base(long n, char *base_to)
 {
-	int	size;
-	int	n_malloc;
+	int		size;
+	int		j;
+	long	len_base_to;
+	char	*nbr_converted;
 
-	size = 1;
+	len_base_to = strpos(base_to, 0);
+	size = get_malloc_size(n, len_base_to);
+	nbr_converted = (char *) malloc(size * sizeof(char));
+	if (nbr_converted == NULL)
+		return (NULL);
+	if (n < 0)
+	{
+		nbr_converted[0] = '-';
+		n = -n;
+	}
+	nbr_converted[size - 1] = '\0';
+	j = 0;
+	while (n > 0 && size - 2 - j >= 0)
+	{
+		nbr_converted[size - 2 - j] = base_to[n % len_base_to];
+		n = n / len_base_to;
+		j++;
+	}
+	return (nbr_converted);
+}
+
+int	get_malloc_size(long n, int len_base)
+{
+	int		size;
+	long	n_malloc;
+
+	size = 1; 
 	if (n < 0)
 	{
 		n_malloc = -n;
 		size++;
 	}
+	else if (n == 0)
+		return (2);
 	else
 		n_malloc = n;
 	while (n_malloc != 0)
@@ -111,7 +123,11 @@ int	strpos(char *str, char c)
 
 	pos = 0;
 	while (str[pos] != c)
+	{
+		if (c != '\0' && str[pos] == '\0')
+			return (-1);
 		pos++;
+	}
 	return (pos);
 }
 
@@ -149,7 +165,8 @@ int	strpos(char *str, char c)
 // }
 
 // int main(int argc, char *argv[]) {
-//     if (argc != 4) {
+//     if (argc != 4) 
+// 	{
 //         printf("Usage: %s <input> <base_from> <base_to>\n", argv[0]);
 //         return 1;
 //     }
@@ -159,9 +176,93 @@ int	strpos(char *str, char c)
 //     char *base_to = argv[3];
 
 //     printf("CONVERSION\n\n");
-//     printf("Input: %s\nBase From: %s\nBase To: %s\nResult: %s\n", 
-//             input, base_from, base_to, 
-// 			ft_convert_base(input, base_from, base_to));
+//     printf("Input: %s\nBase From: %s\nBase To: %s\nResult: %s\n", input, base_from, base_to, ft_convert_base(input, base_from, base_to));
 
+//     return 0;
+// }
+
+// int main(void) {
+    
+// 	char *inputs[] = {"       -+-+---+--2147483647", "101010", "1 0",
+// 		"f7a25", "-arnaud", "<~!~>"};
+    
+// 	char *bases[] = {"arnaud", "0+1", "0 1", "audrey", "0123456789",
+// 		"0123456789abcdef", "abcdefghijklmnopqrstuvwxyz",
+// 		"{}[]<>~!^*()_=", "wtyuioahxvm"};
+
+// 	int i, j, k;
+// 	i = 0; j = 0; k = 0;
+//     printf("CONVERSION\n\n");
+
+// 	while (i < 6)
+// 	{
+// 		while (j < 9)
+// 		{
+// 			while (k < 9)
+// 			{
+// 				long value = ft_atoi_base(inputs[i], bases[j]);
+// 				if (!(chkbse_getsz(bases[j]) <= 1 || chkbse_getsz(bases[k]) <= 1) && value != 0)
+// 				{
+// 					char	*nbr_converted;
+// 					nbr_converted = ft_putnbr_base(value, bases[k]);
+// 					//printf("\tbase_from[%d]\tinput[%d]\tbase_to[%d]\n", i, j, k);
+// 					printf("-------------------------------------------------------------------------------\n{%s}\n\t\t\t> %s \n\t\t\t\t\t\t<%ld>\n{%s}\n\t\t\t> %s\n\n", bases[j], inputs[i], value, bases[k], nbr_converted);
+// 				}
+// 				else
+// 				{
+// 					// printf("KO |%ld| : bases[%d](%d) : %s, inputs[%d], bases[%d](%d) : %s\n",
+// 					// 	value, j,chkbse_getsz(bases[j]), bases[j], i, k, chkbse_getsz(bases[k]), bases[k]);
+// 				}
+// 				k++;
+// 			}
+// 			j++;
+// 			k = 0;
+// 		}
+// 		i++;
+// 		j = 0;
+// 	}
+//     return 0;
+// }
+
+// int main(void) {
+    
+// 	// char *inputs[] = {"       -+-+---+--2147483647", "101010", "1 0",
+// 	// 	"f7a25", "-arnaud", "<~!~>"};
+// 	char *inputs[] = {"101010", "53124", "-facade", "^~!~^"};
+    
+// 	// char *bases[] = {"arnaud", "0+1", "0 1", "audrey", "0123456789",
+// 	// 	"0123456789abcdef", "abcdefghijklmnopqrstuvwxyz",
+// 	// 	"{}[]<>~!^*()_=", "wtyuioahxvm"};
+
+// 	char *bases[] = {"abcdef", "012345", "~!^*=", "france", "01"};
+
+// 	int i, a, b;
+// 	i = 0; a = 0; b = 0;
+//     printf("CONVERSION\n\n");
+
+// 	while (i < 4)
+// 	{	
+// 		a = 0; b = 0;
+// 		printf("\n\n\n\n\n\n****************************************%s****************************************\n", inputs[i]);
+// 		printf("\nbase_from\t");
+// 		while (a < 5)
+// 			printf("%s\t\t", bases[a++]);
+// 		printf("\nvalue\t\t");
+// 		a= 0;
+// 		while (a < 5)
+// 			printf("%ld\t\t", ft_atoi_base(inputs[i], bases[a++]));
+// 		printf("\n");
+// 		while (b < 5)
+// 		{
+// 			printf("----------------------------------------------------------------------------------------------\n");
+// 			printf("%s\t\t", bases[b]);
+// 			a = 0;
+// 			while (a < 5)
+// 				printf("%s\t\t", ft_convert_base(inputs[i], bases[a++], bases[b]));
+// 			printf("\n");
+// 			b++;
+// 		}
+// 		i++;
+// 	}
 //     return 0;
 // }
